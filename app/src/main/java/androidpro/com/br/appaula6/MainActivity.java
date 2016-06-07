6f47429b05e3fabbdb2aed37d6c0af9fd3db68af
dfujise <dfujise@yahoo.com.br>
2016-06-07 08:29:13 -0300

    commit formulario main

--- app/src/main/java/androidpro/com/br/appaula6/MainActivity.java
+++ app/src/main/java/androidpro/com/br/appaula6/MainActivity.java
@@ -1,7 +1,12 @@
 package androidpro.com.br.appaula6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static androidpro.com.br.appaula6.R.id.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bChamar = (Button) findViewById(button);
+        bChamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FormulariosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
